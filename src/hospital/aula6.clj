3102c0de970739ec282fb69f25a3161d5e12925a
guscrat <gustavoapostolic@gmail.com>
2025-01-14 20:39:35 -0300

    on the way - aula6 (future)

--- src/hospital/aula6.clj
+++ src/hospital/aula6.clj
@@ -3,11 +3,18 @@
   (:require
    [hospital.model :as h.model]))
 
+(defn cabe-na-fila?
+  [fila]
+  (-> fila
+      count
+      (< 5)))
 
 (defn chega-em
   "Funcao pura a ser chamada por tras das macros BANG (!)"
   [fila pessoa]
-  (conj fila pessoa))
+  (if (cabe-na-fila? fila)
+    (conj fila pessoa)
+    (throw (ex-info "Fila ja esta cheia" {:tentando-adicionar pessoa}))))
 
 (defn chega-em-ref-set!
   "Troca de referencia via ref-set.
@@ -45,11 +52,41 @@
                       :laboratorio1 (ref h.model/fila-vazia)
                       :laboratorio2 (ref h.model/fila-vazia)
                       :laboratorio3 (ref h.model/fila-vazia)}]
-    
+
     ; dosync --> Starta uma transaciton para utilizacao das refs
     (dosync
-     (chega-em-alter! meu-hospital "guilherme"))
+     (chega-em-alter! meu-hospital "guilherme")
+     (chega-em-alter! meu-hospital "maria")
+     (chega-em-alter! meu-hospital "lucia")
+     (chega-em-alter! meu-hospital "daniela")
+     (chega-em-alter! meu-hospital "ana")
+    ;;  (chega-em-alter! meu-hospital "paulo")
+     )
 
     (pprint meu-hospital)))
 
 (simula-um-dia)
+
+(defn async-chega-em! [hospital pessoa]
+  (future
+    (.start (Thread. (fn [] (Thread/sleep 8000))))
+    (println "Tentando fazer o dosync" 15)
+    (dosync
+     (println "Tentando o codgo sincronizado" pessoa)
+     (chega-em-alter! hospital pessoa))))
+
+
+(defn simula-um-dia-async
+  "Cria um hospital com filas de espera por laboratorio ou apenas lobby"
+  []
+  (let [meu-hospital {:espera (ref h.model/fila-vazia)
+                      :laboratorio1 (ref h.model/fila-vazia)
+                      :laboratorio2 (ref h.model/fila-vazia)
+                      :laboratorio3 (ref h.model/fila-vazia)}]
+    (dotimes [pessoa 10](async-chega-em! meu-hospital pessoa))
+    ))
+    
+(simula-um-dia-async)
+
+(println (future 15))
+(println (future (Thread/sleep (rand 5000)) 15))
